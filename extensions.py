e08839da10afe0778418e5aa0e0e640aa9b52bb7
Name <email@example.ru>
2023-03-01 23:29:34 +0300

    rename files +++

--- extensions.py
+++ extensions.py
@@ -12,16 +12,16 @@ class Convertor:
     @staticmethod
     def convert(base: str, sym: str, amount: str):
         try:
-            quote_ticker = exchanges[base.lower()]
+            base_key = exchanges[base.lower()]
         except KeyError:
             raise APIException(f'Валюта {base} не найдена!')
 
         try:
-            base_ticker = exchanges[sym.lower()]
+            sym_key = exchanges[sym.lower()]
         except KeyError:
             raise APIException(f'Валюта {sym} не найдена!')
 
-        if quote_ticker == base_ticker:
+        if base_key == sym_key:
             raise APIException(f'Невозможно перевести одинаковые валюты {base}!')
 
         try:
@@ -29,9 +29,11 @@ class Convertor:
         except ValueError:
             raise APIException(f'Не удалось обработать количество {amount}!')
 
-        r = requests.get(f"https://api.exchangeratesapi.io/latest?base={quote_ticker}&symbols={base_ticker}")
+        # r = requests.get(f"https://api.exchangeratesapi.io/latest?base={quote_ticker}&symbols={base_ticker}")
+        r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={sym_key}&tsyms={base_key}")
         resp = json.loads(r.content)
-        new_price = resp['rates'][base_ticker] * amount
+        new_price = resp['rates'][base_key] * amount
         new_price = round(new_price, 3)
         message = f'Цена {amount} {base} в {sym} : {new_price}'
         return message
+
